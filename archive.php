9fb5a8c64750279d4026681499bba787e7fcba59
Kanishk Kunal <kanishk.kunal@hotmail.com>
2016-01-28 20:36:37 +0530

    Featured Items archive

--- archive.php
+++ archive.php
@@ -24,11 +24,24 @@ function kkthemes_archive_setup_document() {
 
 
 	// Posts grid
-	beans_add_attribute( 'beans_post', 'class', 'tm-post-grid uk-grid uk-grid-medium' );
-	beans_add_attribute( 'beans_post_image', 'class', 'uk-width-medium-1-2');
-	beans_wrap_markup( 'beans_post_header', 'kkthemes_post_preview', 'div', array(
-	  'class' => 'tm-post-preview uk-width-medium-1-2'
-  ));
+	if(is_featured_items_archive()) {
+		beans_remove_attribute('beans_post', 'class', 'uk-panel-box');
+		beans_add_attribute( 'beans_content', 'class', 'tm-posts-grid uk-grid uk-grid-width-small-1-2' );
+		beans_add_attribute( 'beans_content', 'data-uk-grid-margin', '' );
+		beans_add_attribute( 'beans_content', 'data-uk-grid-match', "{target:'.uk-panel'}" );
+		beans_wrap_inner_markup( 'beans_post_body', 'kkthemes_post_panel', 'div', array(
+			'class' => 'uk-panel uk-panel-box'
+		) );
+
+		beans_remove_attribute('beans_post', 'class', 'uk-article');
+	}
+	else {
+		beans_add_attribute( 'beans_post', 'class', 'tm-post-grid uk-grid uk-grid-medium' );
+		beans_add_attribute( 'beans_post_image', 'class', 'uk-width-medium-1-2');
+		beans_wrap_markup( 'beans_post_header', 'kkthemes_post_preview', 'div', array(
+		  'class' => 'tm-post-preview uk-width-medium-1-2'
+	  ));
+	}
 
 	// Post content
 	beans_remove_action( 'beans_post_content' );
